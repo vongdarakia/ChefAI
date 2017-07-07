374bd7034d3cde50a96a63958c8dd0ed6f170d8c
vongdarakia <vongdarakia@gmail.com>
2017-07-06 22:16:44 -0700

    change to return json

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.status(200).send('Hello, Akia');
+	// res.status(200).send('Hello, Akia');
+	return res.json({
+		speech: "something",
+		displayText: "Hello, Akia",
+		source: 'Chef AI'
+
+	});
 });
 
 if (module === require.main) {
